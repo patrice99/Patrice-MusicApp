787d673c63198c45c998d71c5a0d08b0ebba7b91
Patrice Gill <pgill30@fb.com>
2020-07-13 19:17:21 -0400

    Initialize and define fields for User.java

--- app/src/main/java/com/example/patrice_musicapp/models/User.java
+++ app/src/main/java/com/example/patrice_musicapp/models/User.java
@@ -1,4 +1,20 @@
 package com.example.patrice_musicapp.models;
 
 public class User {
+    public static final String KEY_USERNWME = "username";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_BIO = "bio";
+    public static final String KEY_POST_COUNT = "postCount";
+    public static final String KEY_LOCATION = "location";
+    public static final String KEY_FOLLOWER_COUNT = "followerCount";
+    public static final String KEY_FOLLOWING_COUNT= "followingCount";
+    public static final String KEY_FOLLOWERS = "followers";
+    public static final String KEY_FOLLOWING = "following";
+    public static final String KEY_GENRE = "genre";
+    public static final String KEY_INSTRUMENT = "instrument";
+    public static final String KEY_HOUR_RATE = "hourRate";
+    public static final String KEY_SOLO_ARTIST = "soloArtist";
+
+    
 }
