f0017d976ec78d6890cd96ff80dc9e26c9729b72
Patrice Gill <pgill30@fb.com>
2020-07-14 16:24:11 -0400

    Initialize and define BottomNavigationView in MainActivity

--- app/src/main/java/com/example/patrice_musicapp/activities/MainActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/MainActivity.java
@@ -1,24 +1,19 @@
 package com.example.patrice_musicapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import com.example.patrice_musicapp.R;
-import com.example.patrice_musicapp.models.User;
-import com.parse.ParseObject;
-import com.parse.ParseUser;
-
-import static com.parse.ParseUser.*;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logInInBackground("patrice", "password");
+
 
     }
 }
