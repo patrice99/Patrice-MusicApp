30cf561de22723baef9d3def2672b1ee824e4493
Patrice Gill <pgill30@fb.com>
2020-07-14 18:20:31 -0400

    Inflated item_post XML in PostAdapter

--- app/src/main/java/com/example/patrice_musicapp/adapters/PostAdapter.java
+++ app/src/main/java/com/example/patrice_musicapp/adapters/PostAdapter.java
@@ -1,12 +1,14 @@
 package com.example.patrice_musicapp.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.patrice_musicapp.R;
 import com.example.patrice_musicapp.models.Post;
 
 import java.util.List;
@@ -23,18 +25,19 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
 
     @NonNull
     @Override
-    public PostAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return posts.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
