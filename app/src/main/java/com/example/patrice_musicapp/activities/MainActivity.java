d78ef3c72dd73dca8412058d361c73db2a165355
Patrice Gill <pgill30@fb.com>
2020-07-14 16:43:45 -0400

    Added drawables as icons

--- app/src/main/java/com/example/patrice_musicapp/activities/MainActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/MainActivity.java
@@ -14,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
 
     }
 }
