62918f04898e9882f9982b4b01098bf053944a90
Patrice Gill <pgill30@fb.com>
2020-07-14 18:14:12 -0400

    Implemented adapter methods and created a ViewHolder inner class

--- app/src/main/java/com/example/patrice_musicapp/PostAdapter.java
+++ app/src/main/java/com/example/patrice_musicapp/PostAdapter.java
@@ -1,4 +1,46 @@
 package com.example.patrice_musicapp;
 
-public class PostAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.patrice_musicapp.models.Post;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+    Context context;
+    List<Post> posts;
+
+    //constructor
+    public PostAdapter(Context context, List<Post> posts){
+        this.context = context;
+        this.posts = posts;
+    }
+
+    @NonNull
+    @Override
+    public PostAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
