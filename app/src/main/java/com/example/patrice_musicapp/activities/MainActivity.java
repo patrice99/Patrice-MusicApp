82c0468f41ed4cc6c2a428f91b58810209cfd5aa
Patrice Gill <pgill30@fb.com>
2020-07-14 17:57:06 -0400

    Set up the Frame Manager to switch between Fragments based on the menu icon clicked

--- app/src/main/java/com/example/patrice_musicapp/activities/MainActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/MainActivity.java
@@ -1,20 +1,63 @@
 package com.example.patrice_musicapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.example.patrice_musicapp.R;
+import com.example.patrice_musicapp.fragments.DiscoverFragment;
+import com.example.patrice_musicapp.fragments.FeedFragment;
+import com.example.patrice_musicapp.fragments.MapFragment;
+import com.example.patrice_musicapp.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_feed:
+                        fragment = new FeedFragment();
+                        break;
+                    case R.id.action_discover:
+                        fragment = new DiscoverFragment();
+                        break;
+                    case R.id.action_map:
+                        fragment = new MapFragment();
+                    case R.id.action_profile:
+                    default:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                fragmentManager
+                        .beginTransaction()
+                        .replace(R.id.flContainer, fragment)
+                        .commit();
+                return true;
+            }
+        });
+
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_feed);
+
+
 
 
 
