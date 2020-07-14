a2946ebe1c541ed327b0827effaeff9997c979b2
Patrice Gill <pgill30@fb.com>
2020-07-14 11:29:56 -0400

    Added Instruments Enum

---
+++ app/src/main/java/com/example/patrice_musicapp/models/Instruments.java
@@ -0,0 +1,32 @@
+package com.example.patrice_musicapp.models;
+
+public enum Instruments {
+    ACCORDIAN,
+    ACOUSTIC_GUITAR,
+    BAG_PIPES,
+    BANJO,
+    BASS_GUITAR,
+    BASSOON,
+    BELL,
+    CELLO,
+    CLARINET,
+    DOUBLE_BASS,
+    DRUMS,
+    ELECTRIC_GUITAR,
+    FLUTE,
+    FRENCH_HORN,
+    HARMONICA,
+    KEYBOARD,
+    OBOE,
+    ORGAN,
+    PIANO,
+    SAXOPHONE,
+    STEEL_PAN,
+    TROMBONE,
+    TRUMPET,
+    TUBA,
+    VIOLA,
+    VIOLIN,
+    VOCALS,
+    XYLOPHONE
+}
