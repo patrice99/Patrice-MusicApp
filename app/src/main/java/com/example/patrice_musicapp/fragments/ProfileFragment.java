2ab16e018fc8e3811c07ce15658296ff316e7943
Patrice Gill <pgill30@fb.com>
2020-07-15 18:56:33 -0400

    Added Tab view for Posts and Portfolio in Profile Fragment

--- app/src/main/java/com/example/patrice_musicapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/patrice_musicapp/fragments/ProfileFragment.java
@@ -2,19 +2,26 @@ package com.example.patrice_musicapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.patrice_musicapp.R;
+import com.example.patrice_musicapp.models.Portfolio;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class ProfileFragment extends Fragment {
     private Toolbar toolbar;
+    private TabLayout tabLayout;
+    FragmentManager fragmentManager;
 
 
     @Override
@@ -29,6 +36,46 @@ public class ProfileFragment extends Fragment {
         toolbar.setTitle("Profile");
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        fragmentManager = getFragmentManager();
+        tabLayout = view.findViewById(R.id.tabLayout2);
+        final Fragment fragmentProfilePosts = new ProfilePostsFragment();
+        final Fragment fragmentPortfolio = new PortfolioFragment();
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                if (position == 0) {
+                    //Events positionSelected
+                    //go to EventsPost Fragment
+                    fragmentManager.beginTransaction().replace(R.id.flContainer3, fragmentProfilePosts).commit();
+                } else {
+                    //Maps Position Selected
+                    fragmentManager.beginTransaction().replace(R.id.flContainer3, fragmentPortfolio).commit();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+
+        });
+        tabLayout.getTabAt(1).select();
+        tabLayout.getTabAt(0).select();
+
+
+
     }
 
 }
