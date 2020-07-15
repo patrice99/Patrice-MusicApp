c49d82be997b056d10bdc87f5e36c79d3104d937
Patrice Gill <pgill30@fb.com>
2020-07-15 13:10:09 -0400

    Adding custom toolbar to FeedFragment

--- app/src/main/java/com/example/patrice_musicapp/fragments/FeedFragment.java
+++ app/src/main/java/com/example/patrice_musicapp/fragments/FeedFragment.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.patrice_musicapp.R;
 import com.example.patrice_musicapp.activities.ComposeActivity;
@@ -17,11 +19,19 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FeedFragment extends Fragment {
     private FloatingActionButton fab;
+    private Toolbar toolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+
+        toolbar = (Toolbar) view.findViewById(R.id.toolbar_feed);
+        if (toolbar != null) {
+            ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        }
+        toolbar.setTitle("Feed");
+        return view;
     }
 
     @Override
