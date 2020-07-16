3329ecb4403302008bbf958be56521c052bfd1b2
Patrice Gill <pgill30@fb.com>
2020-07-16 12:19:53 -0400

    Bind views in SignUpActivity and allowed user to be signed up based on their username, email and password

--- app/src/main/java/com/example/patrice_musicapp/activities/SignUpActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/SignUpActivity.java
@@ -2,15 +2,74 @@ package com.example.patrice_musicapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.patrice_musicapp.R;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
+    public static final String TAG = SignUpActivity.class.getSimpleName();
+    private EditText etUsername;
+    private EditText etPassword;
+    private EditText etEmail;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick SignUp button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String email = etEmail.getText().toString();
+                signUpUser(username, password, email);
+            }
+        });
     }
+
+    private void signUpUser(String username, String password, String email) {
+        Log.i(TAG, "Attempting to sign up user" + username);
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    goMainActivity();
+                } else {
+                    Log.e(TAG, "Sign Up Failed", e);
+                }
+            }
+        });
+
+
+    }
+
+    protected void goMainActivity() {
+        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
