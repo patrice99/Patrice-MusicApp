f4e1f07717bf06bc7847d0c6b3df9e3161fdb5f2
Patrice Gill <pgill30@fb.com>
2020-07-15 20:19:25 -0400

    Found rvProfilePosts in ProfileFragment

--- app/src/main/java/com/example/patrice_musicapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/patrice_musicapp/fragments/ProfileFragment.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,26 +21,24 @@ import com.google.android.material.tabs.TabLayout;
 
 public class ProfileFragment extends Fragment {
     private Toolbar toolbar;
-    FragmentManager fragmentManager;
-
+    private RecyclerView rvProfilePosts;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         toolbar =view.findViewById(R.id.toolbar_profile);
         if(toolbar != null){
             ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
         }
         toolbar.setTitle("Profile");
 
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
+        rvProfilePosts = view.findViewById(R.id.rvProfilePosts);
 
     }
 
