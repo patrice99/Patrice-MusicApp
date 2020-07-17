49acb994817b5976b104daa19c9c9c15218b0ddf
Patrice Gill <pgill30@fb.com>
2020-07-16 22:35:55 -0400

    Added onClickListener on logout cardview in Settings to logout user in bacground and fire intent to LoginActivity

--- app/src/main/java/com/example/patrice_musicapp/activities/SettingsActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/SettingsActivity.java
@@ -2,8 +2,12 @@ package com.example.patrice_musicapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.widget.Toolbar;
@@ -12,6 +16,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.patrice_musicapp.R;
+import com.example.patrice_musicapp.models.User;
+import com.parse.LogOutCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import java.util.TreeSet;
 
 public class SettingsActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -30,6 +41,20 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Log out user
+                ParseUser.logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null){
+                            Log.e(SettingsActivity.class.getSimpleName(), "Issues with Logging out" ,e);
+                            return;
+                        }
+                        //fire an intent to LoginActivity
+                        Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
+                        startActivity(intent);
+
+                    }
+                });
+
             }
         });
 
