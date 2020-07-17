badfd64bef43dd66b265c9e5436ea9611cecf97e
Patrice Gill <pgill30@fb.com>
2020-07-17 15:53:15 -0400

    create getters and setters for the Event model class

--- app/src/main/java/com/example/patrice_musicapp/models/Event.java
+++ app/src/main/java/com/example/patrice_musicapp/models/Event.java
@@ -1,11 +1,60 @@
 package com.example.patrice_musicapp.models;
 
+import com.parse.Parse;
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @ParseClassName("Event")
 public class Event extends ParseObject {
+    public static final String KEY_HOST= "host";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_LOCATION = "location";
 
+    public ParseUser getHost() {
+        return getParseUser(KEY_HOST);
+    }
+
+    public void setHost(ParseUser parseUser){
+        put(KEY_HOST, parseUser)
+    }
+
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile image){
+        put(KEY_IMAGE, image);
+    }
+
+    public String getName() {
+        return getString(KEY_NAME);
+    }
+
+    public void setName(String name){
+        put(KEY_NAME, name);
+    }
+
+    public String getDescription() {
+        return getString(KEY_NAME);
+    }
+
+    public void setDescription(String description){
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public ParseGeoPoint getLocation() {
+        return getParseGeoPoint(KEY_LOCATION);
+    }
+
+    public void setLocation(ParseGeoPoint location){
+        put(KEY_LOCATION, location);
+
+    }
 
 
 
