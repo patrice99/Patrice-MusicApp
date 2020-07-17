88e35b7b3ad4106a933d0adfe84ec741542b39ad
Patrice Gill <pgill30@fb.com>
2020-07-17 13:48:47 -0400

    Added a new item_event XML file

--- app/src/main/java/com/example/patrice_musicapp/adapters/EventAdapter.java
+++ app/src/main/java/com/example/patrice_musicapp/adapters/EventAdapter.java
@@ -9,6 +9,9 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.patrice_musicapp.R;
+import com.example.patrice_musicapp.models.Event;
+
+import java.util.List;
 
 public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> {
     Context context;
@@ -22,24 +25,29 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> 
     @NonNull
     @Override
     public EventAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
-        return view;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_event, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull EventAdapter.ViewHolder holder, int position) {
-
+        Event event = events.get(position);
+        holder.bind(event);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return events.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+        }
+
+        public void bind(Event event) {
+            //bind views for events
         }
     }
 }
--- app/src/main/java/com/example/patrice_musicapp/models/Event.java
+++ app/src/main/java/com/example/patrice_musicapp/models/Event.java
@@ -1,4 +1,5 @@
 package com.example.patrice_musicapp.models;
 
 public class Event {
+
 }
