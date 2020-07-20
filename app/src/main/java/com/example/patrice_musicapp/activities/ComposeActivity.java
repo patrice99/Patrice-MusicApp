d22b08be0b569a97d6564cc3b667503cf8860104
Patrice Gill <pgill30@fb.com>
2020-07-20 16:19:39 -0400

    Added a onLaunchCamera method to the ComposeActivity to fire an intent to the device's camera, wrap the image file into a content provider and return back to the ComposeActivity when done

--- app/src/main/java/com/example/patrice_musicapp/activities/ComposeActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/ComposeActivity.java
@@ -2,13 +2,25 @@ package com.example.patrice_musicapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
 import com.example.patrice_musicapp.R;
 
+import java.io.File;
+
 public class ComposeActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    public String photoFilename = "photo.jpg";
+    private File photoFile;
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,28 @@ public class ComposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose);
         toolbar = (Toolbar) findViewById(R.id.toolbar_compose);
         toolbar.setTitle("Make a feed post!");
+    }
+
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application. implicit intent
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFilename);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(ComposeActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    private File getPhotoFileUri(String photoFilename) {
     }
 }
