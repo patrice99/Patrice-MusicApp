bc3ccac12e8c2902f84d91e401829928d2bded9f
Patrice Gill <pgill30@fb.com>
2020-07-20 12:42:14 -0400

    Get the post intent and set up views in XML

--- app/src/main/java/com/example/patrice_musicapp/activities/PostDetailsActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/PostDetailsActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.patrice_musicapp.R;
+import com.example.patrice_musicapp.models.Post;
 
 public class PostDetailsActivity extends AppCompatActivity {
+    private Post post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_details);
+
+        //get the intent and post passed in
+        post = getIntent().getParcelableExtra("post");
+
+
     }
 }
