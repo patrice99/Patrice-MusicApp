f24b7c14aaf245d62b86672f7726801d70fde8dc
Patrice Gill <pgill30@fb.com>
2020-07-20 17:55:37 -0400

    found views for the btnCapture Image and the btnSubmit and set on click listeners

--- app/src/main/java/com/example/patrice_musicapp/activities/ComposeActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/ComposeActivity.java
@@ -1,17 +1,34 @@
 package com.example.patrice_musicapp.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.FileProvider;
 
 import com.example.patrice_musicapp.R;
+import com.example.patrice_musicapp.models.Post;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.io.File;
 
@@ -20,6 +37,10 @@ public class ComposeActivity extends AppCompatActivity {
     public String photoFilename = "photo.jpg";
     private File photoFile;
     public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    public static final String TAG = ComposeActivity.class.getSimpleName();
+    private ImageView ivPostImage;
+    private EditText etCaption;
+    private ImageButton btnCaptureImage;
 
 
     @Override
@@ -28,17 +49,33 @@ public class ComposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose);
         toolbar = (Toolbar) findViewById(R.id.toolbar_compose);
         toolbar.setTitle("Make a feed post!");
+
+        //find views
+        ivPostImage = findViewById(R.id.ivPostImage);
+        etCaption = findViewById(R.id.etCaption);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+
+        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLaunchCamera();
+            }
+        });
+
+
+
+
+
+
     }
 
-    public void onLaunchCamera(View view) {
-        // create Intent to take a picture and return control to the calling application. implicit intent
+    public void onLaunchCamera() {
+        //create an implicit intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Create a File reference for future access
         photoFile = getPhotoFileUri(photoFilename);
 
         // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
         Uri fileProvider = FileProvider.getUriForFile(ComposeActivity.this, "com.codepath.fileprovider", photoFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
 
@@ -50,6 +87,56 @@ public class ComposeActivity extends AppCompatActivity {
         }
     }
 
-    private File getPhotoFileUri(String photoFilename) {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) { //make sure a photo was taken
+                // by this point we have the camera photo on disk
+                //decode the file
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // Load the taken image into a preview
+                ivPostImage.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
+
+    private File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(this.getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    }
+
+    private void savePosts(String caption, ParseUser currentUser, File photoFile) {
+        Post post = new Post();
+        post.setCaption(caption);
+        post.setImage(new ParseFile(photoFile));
+        post.setUser(currentUser);
+        //post.setImage();
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Error while saving", e);
+                    Toast.makeText(ComposeActivity.this, "Error while saving", Toast.LENGTH_SHORT).show();
+                }
+
+                Log.i(TAG, "Post save was successful!");
+                etCaption.setText(""); // clear out edit text so user does not save the same post twice
+                ivPostImage.setImageResource(0); //clear the image view
+            }
+        });
+    }
+
 }
