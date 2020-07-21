254d2f57fa4599233724b554e79f9a2fb9c9185f
Patrice Gill <pgill30@fb.com>
2020-07-20 23:26:40 -0400

    Added onLaunchCamera, onChoosePhoto, getPhotoUri and loadPhotoUri to the ImageUtil class

--- app/src/main/java/com/example/patrice_musicapp/activities/ComposeActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/ComposeActivity.java
@@ -1,9 +1,11 @@
 package com.example.patrice_musicapp.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.ImageDecoder;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -24,6 +26,7 @@ import androidx.core.content.FileProvider;
 
 import com.example.patrice_musicapp.R;
 import com.example.patrice_musicapp.models.Post;
+import com.example.patrice_musicapp.utils.ImageUtil;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
@@ -36,10 +39,6 @@ import java.io.IOException;
 
 public class ComposeActivity extends AppCompatActivity {
     private Toolbar toolbar;
-    public String photoFilename = "photo.jpg";
-    private File photoFile;
-    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
-    public final static int PICK_PHOTO_CODE = 1046;
     public static final String TAG = ComposeActivity.class.getSimpleName();
     private ImageView ivPostImage;
     private EditText etCaption;
@@ -65,7 +64,7 @@ public class ComposeActivity extends AppCompatActivity {
         btnCaptureImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onLaunchCamera();
+                ImageUtil.onLaunchCamera(ComposeActivity.this);
             }
         });
 
@@ -81,12 +80,12 @@ public class ComposeActivity extends AppCompatActivity {
                 }
                 //get the user
                 ParseUser currentUser = ParseUser.getCurrentUser();
-                if(photoFile == null || ivPostImage.getDrawable() == null) {
+                if(ImageUtil.photoFile == null || ivPostImage.getDrawable() == null) {
                     Toast.makeText(ComposeActivity.this, "There is no image", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 //save the post into
-                savePosts(description, currentUser, photoFile);
+                savePosts(description, currentUser, ImageUtil.photoFile);
                 //go back to post fragment(which is in MainActivity)
                 Intent intent = new Intent(ComposeActivity.this, MainActivity.class);
                 startActivity(intent);
@@ -96,99 +95,31 @@ public class ComposeActivity extends AppCompatActivity {
         btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onChoosePhoto();
+                ImageUtil.onChoosePhoto(ComposeActivity.this);
             }
         });
 
 
     }
 
-    public void onLaunchCamera() {
-        //create an implicit intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference for future access
-        photoFile = getPhotoFileUri(photoFilename);
-
-        // wrap File object into a content provider
-        Uri fileProvider = FileProvider.getUriForFile(ComposeActivity.this, "com.musicApp.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-
-    private File getPhotoFileUri(String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(this.getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(TAG, "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        return new File(mediaStorageDir.getPath() + File.separator + fileName);
-    }
-
-
-    //chosing photo from gallery
-    private void onChoosePhoto() {
-        // Create intent for picking a photo from the gallery
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-        // Create a File reference for future access
-        photoFile = getPhotoFileUri(photoFilename);
-
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Bring up gallery to select a photo
-            startActivityForResult(intent, PICK_PHOTO_CODE);
-        }
-    }
-
-
-    public Bitmap loadFromUri(Uri photoUri) {
-        Bitmap image = null;
-        try {
-            // check version of Android on device
-            if(Build.VERSION.SDK_INT > 27){
-                // on newer versions of Android, use the new decodeBitmap method
-                ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), photoUri);
-                image = ImageDecoder.decodeBitmap(source);
-            } else {
-                // support older versions of Android by using getBitmap
-                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return image;
-    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+        if (requestCode == ImageUtil.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) { //make sure a photo was taken
                 // by this point we have the camera photo on disk
                 //decode the file
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                Bitmap takenImage = BitmapFactory.decodeFile(ImageUtil.photoFile.getAbsolutePath());
                 // Load the taken image into a preview
                 ivPostImage.setImageBitmap(takenImage);
             } else { // Result was a failure
                 Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
-        } else if ((data != null) && requestCode == PICK_PHOTO_CODE) {
+        } else if ((data != null) && requestCode == ImageUtil.PICK_PHOTO_CODE) {
             Uri photoUri = data.getData();
             // Load the image located at photoUri into selectedImage
-            Bitmap selectedImage = loadFromUri(photoUri);
+            Bitmap selectedImage = ImageUtil.loadFromUri(photoUri, ComposeActivity.this);
 
             // Load the selected image into a preview
             ivPostImage.setImageBitmap(selectedImage);
@@ -197,7 +128,7 @@ public class ComposeActivity extends AppCompatActivity {
             // Configure byte output stream
             FileOutputStream outStream = null;
             try {
-                outStream = new FileOutputStream(photoFile);
+                outStream = new FileOutputStream(ImageUtil.photoFile);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
--- app/src/main/java/com/example/patrice_musicapp/utils/ImageUtil.java
+++ app/src/main/java/com/example/patrice_musicapp/utils/ImageUtil.java
@@ -1,4 +1,99 @@
 package com.example.patrice_musicapp.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import androidx.core.content.FileProvider;
+
+import com.example.patrice_musicapp.activities.ComposeActivity;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ImageUtil {
+    public static String photoFilename = "photo.jpg";
+    public static File photoFile;
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    public final static int PICK_PHOTO_CODE = 1046;
+
+
+
+
+    public static void onLaunchCamera(Context context) {
+        //create an implicit intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFilename, context);
+
+        // wrap File object into a content provider
+        Uri fileProvider = FileProvider.getUriForFile(context, "com.musicApp.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(context.getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            ((Activity)context).startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    private static File getPhotoFileUri(String fileName, Context context) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), context.getClass().getSimpleName());
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(context.getClass().getSimpleName(), "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    }
+
+
+
+    //chosing photo from gallery
+    public static void onChoosePhoto(Context context) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        if (intent.resolveActivity(context.getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            ((Activity) context).startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
+
+    public static Bitmap loadFromUri(Uri photoUri, Context context) {
+        Bitmap image = null;
+        try {
+            // check version of Android on device
+            if(Build.VERSION.SDK_INT > 27){
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(context.getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(context.getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+
+
+
+
 }
