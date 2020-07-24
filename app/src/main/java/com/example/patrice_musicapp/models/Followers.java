f396bff7f3e41b47d5c70f7ecc0046f2ae4bc116
Patrice Gill <pgill30@fb.com>
2020-07-24 13:58:53 -0400

    Add addFollower method to Followers model class

--- app/src/main/java/com/example/patrice_musicapp/models/Followers.java
+++ app/src/main/java/com/example/patrice_musicapp/models/Followers.java
@@ -1,19 +1,38 @@
 package com.example.patrice_musicapp.models;
 
+import com.parse.Parse;
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.List;
+
 @ParseClassName("Followers")
 public class Followers extends ParseObject {
     public static final String KEY_SUBJECT_USER = "subjectUser";
-    public static final String KEY_SUBJECT_FOLLOWERS = "followers";
+    public static final String KEY_FOLLOWER = "follower";
 
-    public static String getSubjectUser() {
-        return KEY_SUBJECT_USER;
+    public ParseUser getSubjectUser() {
+        return getParseUser(KEY_SUBJECT_USER);
     }
 
-    public static String getKeySubjectFollowers() {
-        return KEY_SUBJECT_FOLLOWERS;
+    public void setSubjectUser(ParseUser parseUser){
+        put(KEY_SUBJECT_USER, parseUser);
     }
 
 
+    public void addFollower(ParseUser parseUser, Followers follow){
+        //add follower to the follower array
+        put(KEY_SUBJECT_USER, parseUser);
+        put(KEY_FOLLOWER, follow);
+
+    }
+
+    public List<User>getFollowers(ParseUser subjectUser){
+        return null;
+    }
+
+
+    public void deleteFollower(ParseUser parseUser, Followers follow) {
+    }
 }
