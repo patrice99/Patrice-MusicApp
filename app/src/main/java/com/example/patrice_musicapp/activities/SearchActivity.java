0ff851c37902ebcfadcd3ba1652897132bec483d
Patrice Gill <pgill30@fb.com>
2020-07-27 11:12:07 -0400

    Added a search item XML

--- app/src/main/java/com/example/patrice_musicapp/activities/SearchActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/SearchActivity.java
@@ -2,6 +2,8 @@ package com.example.patrice_musicapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.patrice_musicapp.R;
@@ -12,5 +14,18 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+
+    private void doMySearch(String query) {
+
+
     }
 }
