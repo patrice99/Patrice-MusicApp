195892364ce4902e5cc2c71c2bd69c0b879b4d12
Patrice Gill <pgill30@fb.com>
2020-07-27 11:27:17 -0400

    Implement methods in search adapter and added inner Viewholder class

--- app/src/main/java/com/example/patrice_musicapp/adapters/SearchAdapter.java
+++ app/src/main/java/com/example/patrice_musicapp/adapters/SearchAdapter.java
@@ -1,5 +1,38 @@
 package com.example.patrice_musicapp.adapters;
 
-public class SearchAdapter {
-    
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder>{
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+
 }
