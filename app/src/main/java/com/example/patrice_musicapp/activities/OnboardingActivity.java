39e50020e8a2aad51490af19b99c507be526bdf4
Patrice Gill <pgill30@fb.com>
2020-07-28 14:02:32 -0400

    Added onboarding Activity

---
+++ app/src/main/java/com/example/patrice_musicapp/activities/OnboardingActivity.java
@@ -0,0 +1,13 @@
+package com.example.patrice_musicapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
