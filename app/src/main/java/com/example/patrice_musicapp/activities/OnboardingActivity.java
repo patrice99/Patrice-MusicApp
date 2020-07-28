cdc4a324eecfa15e8e79529d1e57668786a92348
Patrice Gill <pgill30@fb.com>
2020-07-28 14:39:50 -0400

    Defined the view pager and smart tab layout in the onboarding activity, set onclick listeners to the skip and next buttons and added a finish onBoarding method to fire an intent to main activity when done and close main activity

--- app/src/main/java/com/example/patrice_musicapp/activities/OnboardingActivity.java
+++ app/src/main/java/com/example/patrice_musicapp/activities/OnboardingActivity.java
@@ -1,16 +1,117 @@
 package com.example.patrice_musicapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.patrice_musicapp.R;
+import com.example.patrice_musicapp.fragments.onboarding.OnboardingFragment1;
+import com.example.patrice_musicapp.fragments.onboarding.OnboardingFragment2;
+import com.example.patrice_musicapp.fragments.onboarding.OnboardingFragment3;
+import com.google.android.material.tabs.TabLayout;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
 
 public class OnboardingActivity extends AppCompatActivity {
+    private ViewPager pager;
+    private SmartTabLayout indicator;
+    private Button skip;
+    private Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        pager = findViewById(R.id.pager);
+        indicator = findViewById(R.id.indicator);
+        skip = findViewById(R.id.skip);
+        next = findViewById(R.id.next);
+
+        FragmentStatePagerAdapter adapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+
+                switch (position) {
+                    case 0 : return new OnboardingFragment1();
+                    case 1 : return new OnboardingFragment2();
+                    case 2 : return new OnboardingFragment3();
+                    default: return null;
+                }
+
+            }
+
+            @Override
+            public int getCount() {
+                //number of onboarding screens
+                return 3;
+            }
+        };
+
+
+        pager.setAdapter(adapter);
+        indicator.setViewPager(pager);
+
+
+        //set on click listeners
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishOnboarding();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(pager.getCurrentItem() == 2) { // The last screen
+                    finishOnboarding();
+                } else {
+                    pager.setCurrentItem(
+                            pager.getCurrentItem() + 1,
+                            true
+                    );
+                }
+            }
+        });
+
+
+        indicator.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 2){
+                    skip.setVisibility(View.GONE);
+                    next.setText("Done");
+                } else {
+                    skip.setVisibility(View.VISIBLE);
+                    next.setText("Next");
+                }
+            }
+        });
+
+    }
+
+    private void finishOnboarding() {
+        // Get the shared preferences
+        SharedPreferences preferences =
+                getSharedPreferences("my_preferences", MODE_PRIVATE);
+
+        // Set onboarding_complete to true
+        preferences.edit()
+                .putBoolean("onboarding_complete",true).apply();
+
+        // Launch the main Activity, called MainActivity
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+
+        // Close the OnboardingActivity
+        finish();
     }
 }
