be5d6a0bf2be6f43343b00308b115b3f6073e131
Patrice Gill <pgill30@fb.com>
2020-08-07 18:40:00 -0400

    added Splash page as a gif.

---
+++ app/src/main/java/com/example/patrice_musicapp/activities/SplashPageActivity.java
@@ -0,0 +1,33 @@
+package com.example.patrice_musicapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.example.patrice_musicapp.R;
+
+import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
+
+public class SplashPageActivity extends AppCompatActivity {
+    public static int SPLASH_SCREEN_TIME_OUT=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_page);
+
+        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
+        setContentView(R.layout.splashpage);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashPageActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
