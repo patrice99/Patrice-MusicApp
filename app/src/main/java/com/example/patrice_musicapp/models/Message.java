6e2ace86c7e77fdbd1e74396c153dcecdaedb8ae
Patrice Gill <pgill30@fb.com>
2020-08-07 13:06:27 -0400

    Added a queryMessages method to the Message model class with compound queries

--- app/src/main/java/com/example/patrice_musicapp/models/Message.java
+++ app/src/main/java/com/example/patrice_musicapp/models/Message.java
@@ -1,8 +1,16 @@
 package com.example.patrice_musicapp.models;
 
+import android.util.Log;
+
+import com.parse.FindCallback;
 import com.parse.ParseClassName;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ParseClassName("Message")
 public class Message extends ParseObject {
@@ -33,5 +41,30 @@ public class Message extends ParseObject {
 
     public void setMessageText(String messageText) {
         put(KEY_MESSAGE_TEXT, messageText);
+    }
+
+
+    public static void queryMessages(ParseUser otherUser, ParseUser currentUser, FindCallback<Message> findCallback){
+        // first AND condition
+        ParseQuery<Message> querySentMessages = ParseQuery.getQuery(Message.class);
+        querySentMessages.whereEqualTo(KEY_SENDING_USER, otherUser);
+        querySentMessages.whereEqualTo(KEY_RECEIVING_USER, currentUser);
+
+        //second AND condition
+        ParseQuery<Message> queryReceivedMessages = ParseQuery.getQuery(Message.class);
+        queryReceivedMessages.whereEqualTo(KEY_RECEIVING_USER, otherUser);
+        queryReceivedMessages.whereEqualTo(KEY_SENDING_USER, currentUser);
+
+        //all queries
+        List<ParseQuery<Message>> queries = new ArrayList<ParseQuery<Message>>();
+        queries.add(queryReceivedMessages);
+        queries.add(querySentMessages);
+
+        //OR condition
+        ParseQuery<Message> mainQuery = ParseQuery.or(queries);
+        mainQuery.include(KEY_RECEIVING_USER);
+        mainQuery.include(KEY_SENDING_USER);
+        mainQuery.addAscendingOrder("createdAt");
+        mainQuery.findInBackground(findCallback);
     }
 }
