5e82311afbd015e587b16f713145f52bc8f3db58
Patrice Gill <pgill30@fb.com>
2020-08-07 00:16:54 -0400

    Added Chat Adapter with a base abstract message viewholder and two children classed called depending on the whether the message is incoming or outcoming

---
+++ app/src/main/java/com/example/patrice_musicapp/adapters/ChatAdapter.java
@@ -0,0 +1,134 @@
+package com.example.patrice_musicapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.patrice_musicapp.R;
+import com.example.patrice_musicapp.models.Message;
+import com.example.patrice_musicapp.models.User;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MessageViewHolder> {
+    private static final int MESSAGE_OUTGOING = 123;
+    private static final int MESSAGE_INCOMING = 321;
+
+    private List<Message> messages;
+    private Context context;
+    private String userId;
+
+    public ChatAdapter(Context context, String userId, List<Message> messages) {
+        messages = messages;
+        this.userId = userId;
+        context = context;
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isMe(position)) {
+            return MESSAGE_OUTGOING;
+        } else {
+            return MESSAGE_INCOMING;
+        }
+    }
+
+    @Override
+    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        if (viewType == MESSAGE_INCOMING) {
+            View contactView = inflater.inflate(R.layout.item_message_incoming, parent, false);
+            return new IncomingMessageViewHolder(contactView);
+        } else if (viewType == MESSAGE_OUTGOING) {
+            View contactView = inflater.inflate(R.layout.item_message_outgoing, parent, false);
+            return new OutgoingMessageViewHolder(contactView);
+        } else {
+            throw new IllegalArgumentException("Unknown view type");
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(MessageViewHolder holder, int position) {
+        Message message = messages.get(position);
+        holder.bindMessage(message);
+    }
+
+    public abstract class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        abstract void bindMessage(Message message);
+    }
+
+    public class IncomingMessageViewHolder extends MessageViewHolder {
+        ImageView imageOther;
+        TextView body;
+        TextView name;
+
+        public IncomingMessageViewHolder(View itemView) {
+            super(itemView);
+            imageOther = itemView.findViewById(R.id.ivProfileOther);
+            body = itemView.findViewById(R.id.tvBody);
+            name = itemView.findViewById(R.id.tvName);
+        }
+
+        @Override
+        public void bindMessage(Message message) {
+            User user = new User(message.getSendingUser());
+            Glide.with(context)
+                    .load(user.getImage())
+                    .circleCrop()
+                    .into(imageOther);
+            body.setText(message.getMessageText());
+            name.setText(message.getSendingUser().getUsername());
+        }
+    }
+
+    public class OutgoingMessageViewHolder extends MessageViewHolder {
+        ImageView imageMe;
+        TextView body;
+
+        public OutgoingMessageViewHolder(View itemView) {
+            super(itemView);
+            imageMe = itemView.findViewById(R.id.ivProfileMe);
+            body = itemView.findViewById(R.id.tvBody);
+        }
+
+        @Override
+        public void bindMessage(Message message) {
+            User user = new User(message.getSendingUser());
+            Glide.with(context)
+                    .load(user.getImage())
+                    .circleCrop()
+                    .into(imageMe);
+            body.setText(message.getMessageText());
+        }
+    }
+
+    private boolean isMe(int position) {
+        Message message = messages.get(position);
+        if (message.getSendingUser().getObjectId().equals(ParseUser.getCurrentUser().getObjectId())){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
